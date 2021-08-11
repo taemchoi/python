b00a756655e2d7c48c422c813802d49555fa8765
choi <never7653@gmail.com>
2021-08-11 17:49:15 +0900

    2일차

---
+++ 2_days.py
@@ -0,0 +1,19 @@
+"""
+위장
+"""
+def solution(clothes):  
+    a= {}
+    for i in clothes:
+        if i[-1] in a:
+            a[i[-1]] +=1
+        else:
+            a[i[-1]]=1
+    count= 1
+
+    for i,j in a.items():
+        count*=(j+1)
+    answer= count-1
+    return answer
+
+
+""""""
